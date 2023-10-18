cf4777dd22f29f13eb2978c237774c9bbf2fea75
Brice Carpentier <brice@carpentier.club>
2023-10-18 10:17:39 +0200

    fix Success and Failure types

--- std/result.ts
+++ std/result.ts
@@ -1,20 +1,20 @@
-export type Failure<C> = {
+export type Failure<C, T> = {
   _tag: "Failure"
   cause: C
 }
 
-export type Success<T> = {
+export type Success<C, T> = {
   _tag: "Success"
   value: T
 }
 
-export type Result<C, T> = Failure<C> | Success<T>
+export type Result<C, T> = Failure<C, T> | Success<C, T>
 
-export const Failure = <T>(cause: T): Failure<T> => {
+export const Failure = <T>(cause: T): Failure<T, never> => {
   throw new Error("not implemented")
 }
 
-export const Success = <T>(value: T): Success<T> => {
+export const Success = <T>(value: T): Success<never, T> => {
   throw new Error("not implemented")
 }
 
